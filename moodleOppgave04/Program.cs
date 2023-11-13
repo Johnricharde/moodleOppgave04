01177d480875c9928a7c206f78496edadd4b42f6
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-13 14:11:12 +0100

    Add project files.

---
+++ moodleOppgave04/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace moodleOppgave04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static int calculate(int num1, int num2)
+            {
+                int sum = num1 + num2;
+
+                return sum;
+            }
+
+            Console.WriteLine($"Sum av \"A\" og \"B\" er: {calculate(2, 2)}");
+        }
+    }
+}
